32bf7011be26447349f7087589cd4ca40c01227f
Jivan <jivan241003@gmail.com>
2023-11-04 16:54:03 +0400

    dependencies added in Program.cs

--- BookManagement/Program.cs
+++ BookManagement/Program.cs
@@ -1,11 +1,22 @@
+using Data.Context;
+using Data.Repositories.Impl;
+using Service.Impl;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<BookManagementContext>();
+builder.Services.AddSingleton<CategoryRepositoryImpl>();
+builder.Services.AddSingleton<BookRepositoryImpl>();
+builder.Services.AddSingleton<CategoryServiceImpl>();
+builder.Services.AddSingleton<BookServiceImpl>();
 
 var app = builder.Build();
 
