2a10e40c4fe63edb0e3fad1845e1f596baa50b7a
Jivan <jivan241003@gmail.com>
2023-11-04 17:00:26 +0400

    Book controller added

---
+++ BookManagement/Controllers/BookController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Service;
+
+namespace BookManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly IBookService bookService;
+        public BookController(IBookService bookService)
+        {
+            this.bookService = bookService;
+        }
+    }
+}
